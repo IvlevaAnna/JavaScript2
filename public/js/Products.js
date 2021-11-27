d3fcb4950d34def7f641db2dc032125bfcc9b3dd
IvlevaAnna <annaiva19@mail.ru>
2021-11-27 22:35:15 +0300

    [JavaScript2-8] Домашнее задание №8.

--- public/js/Products.js
+++ public/js/Products.js
@@ -4,7 +4,7 @@ Vue.component('products',{
             catalogUrl: '',
             products: [],
             filtered: [],
-            imgCatalog: 'https://via.placeholder.com/200x150'
+            imgCatalog: 'https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2678&q=80'
         }
     },
     methods: {
